2c01d355d51d331033ac3cb4d0714064e3d74b4b
Terry Tice <nufyoot@gmail.com>
2013-06-04 15:35:15 -0400

    Tweaking the code a bit.

--- test/account.js
+++ test/account.js
@@ -7,26 +7,34 @@ var apiKey      = '123123123123123123123';
 var api = new createsend({ apiKey: apiKey });
 
 describe('Account', function () {
-  var account;
-
-  before(function () {
-    account = new createsend.Account(api, 'admin@example.com');
-  });
-
   it('should add an administrator', function (done) {
     helper.stubRequest('admins.json', 'add_admin.json');
-    createsend.Account.add(api, 'admin@example.com', 'Test', function (err, admin) {
+    api.account.add('admin@example.com', 'Test', function (err, admin) {
       should.exist(admin.update);
-      admin.email.should.equal('admin@example.com');
+      admin.emailAddress.should.equal('admin@example.com');
       done();
     });
   });
 
-  it('should update an administrator', function (done) {
-    helper.stubRequest('admins.json?email=admin%40example.com', null);
-    account.update('new_email_address2@example.com', 'Admin Name', function (err, admin) {
-      admin.email.should.equal('new_email_address2@example.com');
+  it('should get all administrators', function (done) {
+    helper.stubRequest('admins.json', 'administrators.json');
+    api.account.getAdministrators(function (err, admins) {
+      admins.length.should.equal(2);
+      admins[0].emailAddress.should.equal('admin1@blackhole.com');
+      admins[1].name.should.equal('Admin Two');
       done();
+    });
+  });
+
+  it('should allow update on Administrator object', function (done) {
+    helper.stubRequest('admins.json', 'administrators.json');
+    helper.stubRequest('admins.json?email=admin1%40blackhole.com', null);
+    api.account.getAdministrators(function (err, admins) {
+      admins[0].update('admin45@blackhole.com', 'New Admin Name', function (err, admin) {
+        admin.emailAddress.should.equal('admin45@blackhole.com');
+        admin.name.should.equal('New Admin Name');
+        done();
+      });
     });
   });
 });
