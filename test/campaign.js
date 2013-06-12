561eb3f72b42c1f6401d28ca8039671620407133
Terry Tice <terryt@campaignmonitor.com>
2013-06-12 07:28:06 -0400

    Added tests around campaigns

--- test/campaign.js
+++ test/campaign.js
@@ -127,6 +127,22 @@ describe('Campaigns', function () {
     });
   });
 
+  it('should delete a campaign', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '.json', null);
+    api.campaigns.delete(campaignId, function (err, result) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should unschedule campaign', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/unschedule.json', null);
+    api.campaigns.unschedule(campaignId, function (err, result) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
   it('should get a summary', function (done) {
     helper.stubRequest('campaigns/' + campaignId + '/summary.json', 'campaign_summary.json');
     api.campaigns.getSummary(campaignId, function (err, summary) {
@@ -186,6 +202,114 @@ describe('Campaigns', function () {
       recipients.Results.length.should.equal(20);
       recipients.Results[0].EmailAddress.should.equal('subs+6g76t7t0@example.com');
       recipients.Results[0].ListID.should.equal('a994a3caf1328a16af9a69a730eaa706');
+      done();
+    });
+  });
+
+  it('should get the bounces', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/bounces.json', 'campaign_bounces.json');
+    api.campaigns.getBounces(campaignId, null, function (err, bounces) {
+      bounces.Results.length.should.equal(2);
+      bounces.Results[0].EmailAddress.should.equal('asdf@softbouncemyemail.com');
+      bounces.Results[0].ListID.should.equal('654523a5855b4a440bae3fb295641546');
+      bounces.Results[0].BounceType.should.equal('Soft');
+      bounces.Results[0].Date.should.equal('2010-07-02 16:46:00');
+      bounces.Results[0].Reason.should.equal('Bounce - But No Email Address Returned ');
+      bounces.ResultsOrderedBy.should.equal('date');
+      bounces.OrderDirection.should.equal('asc');
+      bounces.PageNumber.should.equal(1);
+      bounces.PageSize.should.equal(1000);
+      bounces.RecordsOnThisPage.should.equal(2);
+      bounces.TotalNumberOfRecords.should.equal(2);
+      bounces.NumberOfPages.should.equal(1);
+      done();
+    });
+  });
+
+  it('should get opens', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/opens.json', 'campaign_opens.json');
+    api.campaigns.getOpens(campaignId, null, function (err, opens) {
+      opens.Results.length.should == 5
+      opens.Results[0].EmailAddress.should.equal('subs+6576576576@example.com');
+      opens.Results[0].ListID.should.equal('512a3bc577a58fdf689c654329b50fa0');
+      opens.Results[0].Date.should.equal('2010-10-11 08:29:00');
+      opens.Results[0].IPAddress.should.equal('192.168.126.87');
+      opens.Results[0].Latitude.should.equal(-33.8683);
+      opens.Results[0].Longitude.should.equal(151.2086);
+      opens.Results[0].City.should.equal('Sydney');
+      opens.Results[0].Region.should.equal('New South Wales');
+      opens.Results[0].CountryCode.should.equal('AU');
+      opens.Results[0].CountryName.should.equal('Australia');
+      opens.ResultsOrderedBy.should.equal('date');
+      opens.OrderDirection.should.equal('asc');
+      opens.PageNumber.should.equal(1);
+      opens.PageSize.should.equal(1000);
+      opens.RecordsOnThisPage.should.equal(5);
+      opens.TotalNumberOfRecords.should.equal(5);
+      opens.NumberOfPages.should.equal(1);
+      done();
+    });
+  });
+
+  it('should get clicks', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/clicks.json', 'campaign_clicks.json');
+    api.campaigns.getClicks(campaignId, null, function (err, clicks) {
+      clicks.Results.length.should.equal(3);
+      clicks.Results[0].EmailAddress.should.equal('subs+6576576576@example.com');
+      clicks.Results[0].URL.should.equal('http://video.google.com.au/?hl=en&tab=wv');
+      clicks.Results[0].ListID.should.equal('512a3bc577a58fdf689c654329b50fa0');
+      clicks.Results[0].Date.should.equal('2010-10-11 08:29:00');
+      clicks.Results[0].IPAddress.should.equal('192.168.126.87');
+      clicks.Results[0].Latitude.should.equal(-33.8683);
+      clicks.Results[0].Longitude.should.equal(151.2086);
+      clicks.Results[0].City.should.equal('Sydney');
+      clicks.Results[0].Region.should.equal('New South Wales');
+      clicks.Results[0].CountryCode.should.equal('AU');
+      clicks.Results[0].CountryName.should.equal('Australia');
+      clicks.ResultsOrderedBy.should.equal('date');
+      clicks.OrderDirection.should.equal('asc');
+      clicks.PageNumber.should.equal(1);
+      clicks.PageSize.should.equal(1000);
+      clicks.RecordsOnThisPage.should.equal(3);
+      clicks.TotalNumberOfRecords.should.equal(3);
+      clicks.NumberOfPages.should.equal(1);
+      done();
+    });
+  });
+
+  it('should get unsubscribes', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/unsubscribes.json', 'campaign_unsubscribes.json');
+    api.campaigns.getUnsubscribes(campaignId, null, function (err, unsubscribes) {
+      unsubscribes.Results.length.should.equal(1);
+      unsubscribes.Results[0].EmailAddress.should.equal('subs+6576576576@example.com');
+      unsubscribes.Results[0].ListID.should.equal('512a3bc577a58fdf689c654329b50fa0');
+      unsubscribes.Results[0].Date.should.equal('2010-10-11 08:29:00');
+      unsubscribes.Results[0].IPAddress.should.equal('192.168.126.87');
+      unsubscribes.ResultsOrderedBy.should.equal('date');
+      unsubscribes.OrderDirection.should.equal('asc');
+      unsubscribes.PageNumber.should.equal(1);
+      unsubscribes.PageSize.should.equal(1000);
+      unsubscribes.RecordsOnThisPage.should.equal(1);
+      unsubscribes.TotalNumberOfRecords.should.equal(1);
+      unsubscribes.NumberOfPages.should.equal(1);
+      done();
+    });
+  });
+
+  it('should get spam complaints', function (done) {
+    helper.stubRequest('campaigns/' + campaignId + '/spam.json', 'campaign_spam.json');
+    api.campaigns.getSpamComplaints(campaignId, null, function (err, spam) {
+      spam.Results.length.should.equal(1);
+      spam.Results[0].EmailAddress.should.equal('subs+6576576576@example.com');
+      spam.Results[0].ListID.should.equal('512a3bc577a58fdf689c654329b50fa0');
+      spam.Results[0].Date.should.equal('2010-10-11 08:29:00');
+      spam.ResultsOrderedBy.should.equal('date');
+      spam.OrderDirection.should.equal('asc');
+      spam.PageNumber.should.equal(1);
+      spam.PageSize.should.equal(1000);
+      spam.RecordsOnThisPage.should.equal(1);
+      spam.TotalNumberOfRecords.should.equal(1);
+      spam.NumberOfPages.should.equal(1);
       done();
     });
   });
